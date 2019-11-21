a20de66bff995450ace14186dd5982c24097fc64
Chang-Jin <2475477667@qq.com>
2019-11-21 19:22:37 +0800

    feat: 添加基础配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
-module.exports = {
-    entry: {
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
-    output: {}
+    devServer: {
+        port: 8080,
+        open: true,
+        contentBase: './dist'
+    },
+    module: {},
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 }
