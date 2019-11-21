4e80085233e33c13f3d4ab1aab4205c9b05372a4
Chang-Jin <2475477667@qq.com>
2019-11-21 19:37:09 +0800

    feat: 添加html处理

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,28 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    mode: 'development', // 模式
+    entry: './src/index.js', // 入口
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.[hash:8].js', // 打包后文件名
+        path: path.resolve(__dirname, 'dist') // 路径
     },
-    devServer: {
-        port: 8080,
-        open: true,
-        contentBase: './dist'
+    devServer: { // 开发服务器配置
+        port: 8080, // 服务端口号
+        progress: true, // 进度条
+        open: true, // 自动打开浏览器
+        contentBase: './dist' // 服务运行的地址
     },
     module: {},
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html'
+            template: './public/index.html',
+            filename: 'index.html', // 生成文件名
+            minify: {
+                removeAttributeQuotes: true, // 删除双引号
+                collapseWhitespace: true // 折叠为一行
+            },
+            hash: true // 添加hash戳
         })
     ]
 }
